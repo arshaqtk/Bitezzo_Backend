2ee5387449b51342402b6fe625d3942f4c516de2
muhammed  arshaq tk <arshaqtk4@gmail.com>
2025-11-01 10:16:48 +0530

    fix: update CORS origin settings to allow requests from the production URL

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://bitezzo-mern-68gz.vercel.app/",],
     credentials: true,
   })
 );
@@ -23,7 +23,7 @@ app.use(
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://bitezzo-mern-68gz.vercel.app/",],
     methods: ["GET", "POST"],
     credentials: true,
   },
